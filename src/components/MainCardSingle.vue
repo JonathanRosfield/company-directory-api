18ecb4798e6cb49b18688516d031bc36314f4034
Jonathan Hernandez <jonahernandez2001@gmail.com>
2024-11-13 16:44:26 -0600

    connect to API is working

--- src/components/MainCardSingle.vue
+++ src/components/MainCardSingle.vue
@@ -1,12 +1,30 @@
 <script setup>
 import { faker } from '@faker-js/faker'
 
-const firstname = faker.person.firstName();
+/* const firstname = faker.person.firstName();
 const lastname = faker.person.lastName();
 const fullname = faker.person.fullName({ firstName: firstname, lastName: lastname });
 const job_title = faker.person.jobTitle();
 const bio = faker.lorem.lines(2);
-const username = faker.internet.userName({ firstName: firstname, lastName: lastname }).toLowerCase();
+const username = faker.internet.userName({ firstName: firstname, lastName: lastname }).toLowerCase(); */
+
+const props = defineProps({
+    employee: {
+        type: Object,
+        required: true,
+        default: () => {
+            return {
+                _id: "672ae5211747aa36e9d5f076",
+                userName: "jperez",
+                firstName: "Joseph",
+                lastName: "Perez",
+                title: "Computer Science Wizard",
+                quote: "You shall not pass!",
+                __v: 0
+            }
+        }
+    }
+})
 
 
 </script>
@@ -14,13 +32,13 @@ const username = faker.internet.userName({ firstName: firstname, lastName: lastn
 
 <template>
 <!-- {{ fullname }} <br /-->
-<RouterLink :to="{name: 'CardDetails', params: {id: username}}">
+<RouterLink :to="{name: 'CardDetails', params: {id: props.employee._id}}">
 <div class="border border-gray-400 rounded-xl shadow bg-gray-200 h-full">
     <img :src="faker.image.urlLoremFlickr({width: 640, height: 480, category: 'cat'})" class="rounded-t-xl" />
     <div class="p-4">
-        <p class="mb-2 text-2xl font-bold">{{ fullname }}</p>
-        <p class="mb-3 font-normal text-gray-700">{{ job_title }}</p>
-        <p class="mb-3 font-normal text-gray-700">{{ bio }}</p>
+        <p class="mb-2 text-2xl font-bold">{{ props.employee.firstName }} {{ props.employee.lastName }}</p>
+        <p class="mb-3 font-normal text-gray-700">{{ props.employee.title }}</p>
+        <p class="mb-3 font-normal text-gray-700">{{ props.employee.quote }}</p>
     </div>
 </div>
 </RouterLink>
